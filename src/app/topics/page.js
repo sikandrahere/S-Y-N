a015cc576c588815cf02d2cc224b280861b018e3
sikandrahere <148213515+sikandrahere@users.noreply.github.com>
2025-04-18 14:29:49 +0530

    error fix

--- src/app/topics/page.js
+++ src/app/topics/page.js
@@ -1,22 +1,23 @@
-"use client"
-import React, { useState, useEffect } from 'react'
+"use client";
+import React, { useState, useEffect } from "react";
 import { Tabs } from "@/components/ui/tabs";
-import Politics from '@/components/Politics';
-import { TracingBeam } from '@/components/ui/tracing-beam';
-import Confession from '@/components/Confession';
-import Entertainment from '@/components/Entertainment';
-import Unfiltered from '@/components/Unfiltered';
-const page = () => {
+import Politics from "@/components/Politics";
+import { TracingBeam } from "@/components/ui/tracing-beam";
+import Confession from "@/components/Confession";
+import Entertainment from "@/components/Entertainment";
+import Unfiltered from "@/components/Unfiltered";
+
+const TopicsPage = () => {
   const [posts, setPosts] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    const fetchPost = async () => {
+    const fetchPosts = async () => {
       try {
         const response = await fetch("/api/fetch-posts");
         if (!response.ok) {
-          throw new Error("Failed to fetch the post");
+          throw new Error("Failed to fetch the posts");
         }
         const data = await response.json();
         setPosts(data);
@@ -28,7 +29,7 @@ const page = () => {
       }
     };
 
-    fetchPost();
+    fetchPosts();
   }, []);
 
   if (loading) {
@@ -38,31 +39,32 @@ const page = () => {
   if (error) {
     return <div className="text-center mt-20">Error: {error}</div>;
   }
+
   const tabs = [
     {
       title: "Politics",
       value: "politics",
       content: (
-
-        <div className="w-full overflow-y-scroll scrollbar-hidden relative h-full rounded-2xl p-10 font-bold text-white bg-gradient-to-br  from-black to-black border-2 border-gray-800">
+        <div className="w-full overflow-y-scroll scrollbar-hidden relative h-full rounded-2xl p-10 font-bold text-white bg-gradient-to-br from-black to-black border-2 border-gray-800">
           <TracingBeam className="px-6">
-            <p>" No judgement, just your voice on the issues "</p>
+            <p>&quot; No judgement, just your voice on the issues &quot;</p>
             <div>
-              <Politics posts={posts}/>
+              <Politics posts={posts} />
             </div>
           </TracingBeam>
         </div>
-
       ),
     },
     {
       title: "Confession",
       value: "confession",
       content: (
-        <div className="w-full overflow-y-scroll  scrollbar-hidden relative h-full rounded-2xl p-10   font-bold text-white bg-gradient-to-br  from-black to-black border-2 border-gray-800">
+        <div className="w-full overflow-y-scroll scrollbar-hidden relative h-full rounded-2xl p-10 font-bold text-white bg-gradient-to-br from-black to-black border-2 border-gray-800">
           <TracingBeam className="px-6">
-            <p>" Here Your secrets are safe "</p>
-            <div><Confession posts={posts} /></div>
+            <p>&quot; Here Your secrets are safe &quot;</p>
+            <div>
+              <Confession posts={posts} />
+            </div>
           </TracingBeam>
         </div>
       ),
@@ -71,10 +73,12 @@ const page = () => {
       title: "Entertainment",
       value: "entertainment",
       content: (
-        <div className="w-full overflow-y-scroll  scrollbar-hidden relative h-full rounded-2xl p-10  font-bold text-white bg-gradient-to-br from-black to-black border-2 border-gray-800">
+        <div className="w-full overflow-y-scroll scrollbar-hidden relative h-full rounded-2xl p-10 font-bold text-white bg-gradient-to-br from-black to-black border-2 border-gray-800">
           <TracingBeam className="px-6">
-            <p>" Your entertainment, your safe space "</p>
-            <div><Entertainment posts={posts} /></div>
+            <p>&quot; Your entertainment, your safe space &quot;</p>
+            <div>
+              <Entertainment posts={posts} />
+            </div>
           </TracingBeam>
         </div>
       ),
@@ -83,22 +87,23 @@ const page = () => {
       title: "Unfiltered",
       value: "unfiltered",
       content: (
-        <div className="w-full overflow-y-scroll   scrollbar-hidden relative h-full rounded-2xl p-10  font-bold text-white bg-gradient-to-br  from-black to-black border-2 border-gray-800">
+        <div className="w-full overflow-y-scroll scrollbar-hidden relative h-full rounded-2xl p-10 font-bold text-white bg-gradient-to-br from-black to-black border-2 border-gray-800">
           <TracingBeam className="px-6">
-            <p>" Your thoughts, your voice "</p>
-            <div><Unfiltered posts={posts} /></div>
+            <p>&quot; Your thoughts, your voice &quot;</p>
+            <div>
+              <Unfiltered posts={posts} />
+            </div>
           </TracingBeam>
         </div>
       ),
     },
   ];
+
   return (
-    <div className="h-[20rem] md:h-[40rem] [perspective:1000px] relative b flex flex-col max-w-5xl mx-auto w-full  items-start justify-start my-40 ">
-
+    <div className="h-[20rem] md:h-[40rem] [perspective:1000px] relative b flex flex-col max-w-5xl mx-auto w-full items-start justify-start my-40">
       <Tabs tabs={tabs} />
-
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default TopicsPage;
--- src/app/trending/page.js
+++ src/app/trending/page.js
@@ -1,46 +1,45 @@
-"use client"
-import Trending from '@/components/Trending'
-import React, { useState, useEffect } from 'react'
+"use client";
+import Trending from "@/components/Trending";
+import React, { useState, useEffect } from "react";
 
-const page = () => {
+const TrendingPage = () => {
   const [posts, setPosts] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-  
-    useEffect(() => {
-      const fetchPost = async () => {
-        try {
-          const response = await fetch("/api/fetch-posts");
-          if (!response.ok) {
-            throw new Error("Failed to fetch the post");
-          }
-          const data = await response.json();
-          setPosts(data);
-          setLoading(false);
-        } catch (err) {
-          console.error(err);
-          setError(err.message);
-          setLoading(false);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch("/api/fetch-posts");
+        if (!response.ok) {
+          throw new Error("Failed to fetch posts");
         }
-      };
-  
-      fetchPost();
-    }, []);
-  
-    if (loading) {
-      return <div className="text-center mt-20">Loading...</div>;
-    }
-  
-    if (error) {
-      return <div className="text-center mt-20">Error: {error}</div>;
-    }
+        const data = await response.json();
+        setPosts(data);
+        setLoading(false);
+      } catch (err) {
+        console.error(err);
+        setError(err.message);
+        setLoading(false);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
+  if (loading) {
+    return <div className="text-center mt-20">Loading...</div>;
+  }
+
+  if (error) {
+    return <div className="text-center mt-20">Error: {error}</div>;
+  }
+
   return (
     <div>
-        
-        <Trending posts={posts}/>
-
+      <Trending posts={posts} />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default TrendingPage;
